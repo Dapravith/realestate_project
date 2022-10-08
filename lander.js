19861958ee7b3e469b768afa3ec2cdab65558f29
ROTHA DAPRAVITH <90898700+Dapravith@users.noreply.github.com>
2022-10-08 09:22:04 +0700

    Add files via upload

---
+++ lander.js
@@ -0,0 +1,13 @@
+/**
+ * @jsx React.DOM
+ */
+ var Lander = React.createClass({
+    render: function () {
+        var info = "Lorem ipsum dolor sit amet... ";
+        return(
+            <div>
+                <div className="info">{info}</div>
+            </div>
+        );
+    }
+});
